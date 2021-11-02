e3df0f29f96f08c67234d97013e72dbc96e6f683
SP <82333629+Sp1406@users.noreply.github.com>
2021-11-02 11:42:15 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Not in course! This is made by me for the purpose of making the page function better
-
-function download() {
-    alert("This is a page just for practice purposes! So there are no download links available")
-}
-
-function signup() {
-    alert("Thanks for clicking on this button but this webpage is made for practice purposes only!")
-}
